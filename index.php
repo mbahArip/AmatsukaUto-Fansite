9c77f37640d399a497092c5b16f8920f0180a8f3
Arief Rachmawan <kanoon.storage@gmail.com>
2021-02-23 03:02:02 +0700

    initial

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" sizes="32x32" href="Assets\img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="Assets\img\favicon-16x16.png">
+    <meta name="theme-color" content="#67b0e1">
+
+    <!-- Style and Font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Kosugi&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="Assets\style.css">
+
+    <!-- Primary Meta Tags -->
+    <title>天使うと Amatsuka Uto | ファンサイト</title>
+    <meta name="title" content="天使うと Amatsuka Uto | ファンサイト">
+    <meta name="description" content="天使うと、ファンサイト。このウェブサイトは学習目的で作成されました。天使うとは何の関係もありません。">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://metatags.io/">
+    <meta property="og:title" content="天使うと Amatsuka Uto | ファンサイト">
+    <meta property="og:description" content="天使うと、ファンサイト。このウェブサイトは学習目的で作成されました。天使うとは何の関係もありません。">
+    <meta property="og:image" content="Assets\img\thumb.jpg">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://metatags.io/">
+    <meta property="twitter:title" content="天使うと Amatsuka Uto | ファンサイト">
+    <meta property="twitter:description" content="天使うと、ファンサイト。このウェブサイトは学習目的で作成されました。天使うとは何の関係もありません。">
+    <meta property="twitter:image" content="Assets\img\thumb.jpg">
+</head>
+
+<body>
+    <section>
+        <div class="main">
+            <img src="Assets\img\headerv2.png" alt="天使うと Amatsuka Uto | ファンサイト">
+        </div>
+    </section>
+</body>
+
+</html>
